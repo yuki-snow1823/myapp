ddd99a72f2c088f1aa35f77227f51391875b7ec6
yuki-snow1823 <magic.iroha@gmail.com>
2020-02-16 11:24:37 +0900

    [fix]ログインまわり

--- gamecenter/app/controllers/application_controller.rb
+++ gamecenter/app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
  
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+    # ログイン後ホームではなくマイページに行かせる
+  end
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
   def configure_permitted_parameters
--- gamecenter/app/controllers/users_controller.rb
+++ gamecenter/app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user! 
+  before_action :correct_user, only: [:edit, :update]
+
   def index
+    @users = User.all
   end
 
   def show
-    @user = current_user
-    # idじゃなくていいのかな
+    @user = User.find(params[:id])
+    # idじゃなくていいのかな →よくない。idじゃなくてどこにアクセスしても自分のプロフが出る
   end
 
   def edit
@@ -21,6 +25,17 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :introduction, :user_image)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    if current_user != @user
+       redirect_to user_path(current_user.id)
+       # 考える!!userのshow（自分）に行けない、一覧じゃない！
+       #paramsだと結局前のやつを参照しちゃう
+       #正しい人のIDだからこういう表現になるのだと思う、Railsのメソッド。
+       #アクションじゃないからここに書く
+    end
   end
 
 end

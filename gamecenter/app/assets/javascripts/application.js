468f8a4974ab29cdac3cc01c7fbcfe31008a040b
yuki-snow1823 <magic.iroha@gmail.com>
2020-02-19 16:18:26 +0900

    [fix]あとは装飾

--- gamecenter/app/assets/javascripts/application.js
+++ gamecenter/app/assets/javascripts/application.js
@@ -15,4 +15,5 @@
 //= require turbolinks
 //= require_tree .
 //= require jquery
+//= require popper
 //= require bootstrap-sprockets
--- gamecenter/app/controllers/games_controller.rb
+++ gamecenter/app/controllers/games_controller.rb
@@ -7,6 +7,11 @@ class GamesController < ApplicationController
   def push
   end
 
+  # 魔王への扉
+  # どんなに勝っても１しか増えず、悪魔に勝ってから戦っても１にならないようにしたい
   def open
+    current_user.status = 1
+    current_user.save
+    redirect_to action: "answer" 
   end
 end
